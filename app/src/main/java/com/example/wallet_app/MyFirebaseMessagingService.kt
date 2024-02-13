6677e4614418d588e9237f431eb8e8ab410f626e
Artan0 <artanbayram03@gmail.com>
2024-02-13 11:46:15 +0100

    now notifications are saved on database and in activity we have real notifications

--- app/src/main/java/com/example/wallet_app/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/wallet_app/MyFirebaseMessagingService.kt
@@ -6,10 +6,14 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.firestore.FirebaseFirestore
 
 const val channelId = "notification_channel"
 const val channelName = "com.example.wallet_app"
@@ -18,6 +22,34 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if (remoteMessage.notification !=null){
          generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
+
+            // Save the notification in Firestore
+            saveNotificationToFirestore(
+                remoteMessage.notification!!.title!!,
+                remoteMessage.notification!!.body!!
+            )
+        }
+
+    }
+    private fun saveNotificationToFirestore(title: String, message: String) {
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        if (userId != null) {
+            val notification = hashMapOf(
+                "title" to title,
+                "content" to message,
+                "timestamp" to FieldValue.serverTimestamp()
+            )
+
+            FirebaseFirestore.getInstance().collection("notifications")
+                .document(userId)
+                .collection("userNotifications")
+                .add(notification)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("Firestore", "Notification added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Firestore", "Error adding notification", e)
+                }
         }
     }
 
--- app/src/main/java/com/example/wallet_app/NotificationActivity.kt
+++ app/src/main/java/com/example/wallet_app/NotificationActivity.kt
@@ -1,18 +1,24 @@
 package com.example.wallet_app
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wallet_app.adapters.NotificationAdapter
 import com.example.wallet_app.databinding.ActivityNotificationBinding
 import com.example.wallet_app.model.Notification
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class NotificationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNotificationBinding
     private lateinit var notificationAdapter: NotificationAdapter
     private lateinit var backButton:ImageButton
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    private val firestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +33,8 @@ class NotificationActivity : AppCompatActivity() {
         notificationAdapter = NotificationAdapter(getDummyNotifications())
         binding.notificationRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.notificationRecyclerView.adapter = notificationAdapter
+
+        retrieveUserNotifications()
     }
 
     private fun getDummyNotifications(): List<Notification> {
@@ -35,5 +43,31 @@ class NotificationActivity : AppCompatActivity() {
             Notification("Payment Received", "You received a payment", System.currentTimeMillis()),
             Notification("Friend Request", "You have a new friend request", System.currentTimeMillis())
         )
+    }
+
+    private fun retrieveUserNotifications() {
+        if (userId != null) {
+            firestore.collection("notifications")
+                .document(userId)
+                .collection("userNotifications")
+                .orderBy("timestamp", Query.Direction.DESCENDING) // Order by timestamp
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    val notifications = mutableListOf<Notification>()
+                    for (document in querySnapshot.documents) {
+                        val title = document.getString("title") ?: ""
+                        val content = document.getString("content") ?: ""
+                        val timestamp = document.getTimestamp("timestamp")?.toDate()?.time ?: 0L
+
+                        notifications.add(Notification(title, content, timestamp))
+                    }
+
+                    // Update the adapter with real notifications
+                    notificationAdapter.updateData(notifications)
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("Firestore", "Error getting notifications", exception)
+                }
+        }
     }
 }
